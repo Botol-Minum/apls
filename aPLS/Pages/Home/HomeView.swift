ab676bfeecfae49634abad732ff5658dd49a08d3
Hypes-Astro <muhamadalifanwar@gmail.com>
2025-07-13 18:25:03 +0700

    page 1

--- aPLS/Pages/Home/HomeView.swift
+++ aPLS/Pages/Home/HomeView.swift
@@ -13,27 +13,38 @@ struct HomeView: View {
     @StateObject var viewModel = HomeViewModel()
     
     var body: some View {
-        NavigationStack{
-            ScrollView{
-                VStack (alignment: .leading,spacing: 12) {
-                    
-                    ForEach(viewModel.members) {  member in
-                        MemberCard(
-                            name: member.name,
-                            color: Color(member.color)
-                        )
+        NavigationStack {
+            ZStack {
+//               ok ini aku masih bingung wkwkwkwk untuk bulat bulatnya
+// rencana mo ku tarok sini bulet buletnya
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 12) {
+                        
+                        Text("Botol Minum Member’s")
+                            .font(.system(size: 30))
+                            .fontWeight(.bold)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.top, 24)
+                            .padding(.bottom, 8)
+                        
+                        ForEach(viewModel.members) { member in
+                            MemberCard(
+                                name: member.name,
+                                color: Color(member.color)
+                            )
+                        }
                     }
-                    
-                }.padding(EdgeInsets(
-                    top: 0,
-                    leading: 16,
-                    bottom: 16,
-                    trailing: 16)
-                )
-            }.navigationTitle("Members")
+                    .padding(.horizontal, 16)
+                    .padding(.bottom, 16)
+                }
+            }
+            .navigationTitle("")
+            .navigationBarHidden(true)
         }
     }
 }
+
 
 #Preview {
     HomeView()
--- aPLS/Pages/Home/ViewComponents/MemberCard.swift
+++ aPLS/Pages/Home/ViewComponents/MemberCard.swift
@@ -30,12 +30,16 @@ struct MemberCard: View {
                     }
                     
                     VStack (alignment: .leading, spacing: 8){
-                        Text(name)
-                            .font(.title2)
-                            .foregroundStyle(.white)
-                            .frame(maxWidth: .infinity,alignment: .leading)
+                        
+                        
                         
                         if isExpanded {
+                            
+                            Text(name)
+                                .font(.title2)
+                                .foregroundStyle(.white)
+                                .frame(maxWidth: .infinity,alignment: .leading)
+                            
                             VStack (alignment: .leading){
                                 Text("Check the aPLS")
                                     .font(.body)
@@ -45,8 +49,40 @@ struct MemberCard: View {
                                 .background(Color.white)
                                 .cornerRadius(12)
                         }
+                        else{
+                            HStack(spacing:20){
+                                ZStack{
+                                    RoundedRectangle(cornerRadius: 200, style: .continuous)
+                                        .fill(Color.white.opacity(0.4))
+                                        .frame(width:100, height: 100)
+                                    
+                                    Image(DesignImages.mario)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 60)
+                                    
+                                }
+                                VStack(alignment: .leading, spacing: 4) {
+                                    Text(name)
+                                        .font(.system(size: 25, weight: .semibold))
+                                        .foregroundColor(.black)
+                                    
+                                    HStack(spacing: 4) {
+                                        Text("20th")
+                                        Text("|")
+                                        Text("single")
+                                        Text("|")
+                                        Text("sleeper")
+                                    }
+                                    .font(.system(size: 14))
+                                    .foregroundColor(.black.opacity(0.8))
+                                }
+                              
+                            }
+                        }
                         
-                    }.frame(maxWidth: .infinity,alignment: .leading)
+                    }.frame(maxWidth: .infinity ,alignment: .leading)
+                        
                     
                 }.padding(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: -24))
                     .frame(maxHeight: .infinity, alignment: isExpanded ? .bottom : .center)
@@ -55,8 +91,15 @@ struct MemberCard: View {
                 Spacer()
                 
                 VStack (alignment: .trailing){
-                    Image(systemName: DesignIcons.chevronRight)
-                        .foregroundStyle(.white)
+                    Image(systemName: "arrow.up.left.and.arrow.down.right")
+                           .resizable()
+                           .scaledToFit()
+                           .frame(width: 24, height: 24)
+                           .foregroundColor(.black)
+                           .padding(13)
+                           .background(Color.white)
+                           .clipShape(RoundedRectangle(cornerRadius: 12, style: .continuous))
+                           .shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
                     
                     if isExpanded {
                         VStack {
@@ -80,7 +123,7 @@ struct MemberCard: View {
                     .frame(maxHeight: .infinity, alignment: isExpanded ? .top : .center)
                 
             }.padding(EdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 24))
-                .frame(height: isExpanded ? 360 : 80, alignment: isExpanded ? .bottom : .center)
+                .frame(height: isExpanded ? 360 : 120, alignment: isExpanded ? .bottom : .center)
                 .frame(maxWidth: .infinity,alignment: .leading)
                 .background(color)
                 .cornerRadius(isExpanded ? 16 : 12)
